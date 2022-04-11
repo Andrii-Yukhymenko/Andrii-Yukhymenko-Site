1c347e5407c9f8c38e9ef063824045a1f02a0ae6
Andrii Yukhymenko <andriy111224@gmail.com>
2022-04-12 00:10:57 +0300

    Console-bar, ascii

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,7 @@
-import './_vendor';
-import vars from './_vars';
-import './_functions';
-import './_components';
+const input = document.querySelector(".console-bar-form__input");
+const fakeCaret = document.querySelector(".console-bar-form__fake-caret");
+
+input.addEventListener("focus", () => (fakeCaret.style.display = "none"));
+input.addEventListener("focusout", (e) => {
+  e.target.value.length === 0 ? (fakeCaret.style.display = "block") : "";
+});
